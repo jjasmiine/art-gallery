f1ea8d53ae52b09767516831afaf574450dbb27f
lljasmine <84794871+lljasmine@users.noreply.github.com>
2022-02-22 17:53:28 -0600

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+// ()
+
+let images = [...document.querySelectorAll('.img')];
+let slider = document.querySelector('.slider');
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = .05;
+
+window.addEventListener('resize', init);
+
+images.forEach((img, index) => {
+    img.style.backgroundImage = `url(./assets/img/${index + 1}.jpg)`
+})
+
+// t = ease
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+function setTransform(el, transform) {
+    el.style.transform = transform;
+}
+
+function init() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth / images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+}
+
+function animate() {
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTransform(slider, `translateX(-${current}px)`)
+    animateImages();
+    requestAnimationFrame(animate);
+}
+
+function animateImages() {
+    let ratio = current / imageWidth;
+    let intersectionRatioValue;
+
+    images.forEach((image, index) => {
+        intersectionRatioValue = ratio - (index * 0.7);
+        setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
+    })
+}
+
+init();
+animate();
+
